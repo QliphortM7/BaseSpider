9c9bf897b949730848edc0e184d9813d68b7f664
weimaoquan <qliphortm7@gmail.com>
2020-11-02 18:06:40 +0800

    添加excel,尝试其他类别

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import time
 import requests
 import re
 import json
+import xlwt
 from bs4 import BeautifulSoup
+global n
+n = 0
 
 
 def get_url_data(crawl_url):
     page_data = get_page(crawl_url)
-    json_page_data = json.dumps(page_data, ensure_ascii=False)
-    f = open('page_data', 'w', encoding='utf8')
-    f.write(json_page_data)
-    f.close()
+    # save_as_excel(page_data)
 
 
 def get_page(crawl_url):
@@ -27,7 +27,9 @@ def get_page(crawl_url):
     soup = BeautifulSoup(text, 'lxml')
     while len(soup.select('div[class="info"]')) != 0:
         temp_data_list = get_data_bs(soup)
-        data_list = data_list + temp_data_list
+        print(n)
+        if len(temp_data_list) != 0:
+            data_list = data_list + temp_data_list
         if len(soup.select('span[class="next"] a')) != 0:
             next_url = 'http://book.douban.com' + soup.select('span[class="next"] a')[0]['href']
             res = requests.get(next_url, headers=headers)
@@ -35,7 +37,7 @@ def get_page(crawl_url):
             soup = BeautifulSoup(text, 'lxml')
         else:
             break
-    print("有效数据：" + str(len(data_list)))
+    print("完整数据：" + str(len(data_list)))
     return data_list
 
 
@@ -59,26 +61,58 @@ def get_page(crawl_url):
 
 # 使用BeautifulSoup方法
 def get_data_bs(soup):
+    global n
     data_list = []
     for item in soup.select('div[class="info"]'):
+        n += 1
         info_list = item.select('div[class="pub"]')[0].get_text().split('/')
         for i in range(len(info_list)):
             info_list[i] = info_list[i].strip()
         if len(info_list) < 4:
-            return data_list
+            print("/".join(info_list))
+            continue
         rating_nums = item.select('span[class="rating_nums"]')
         temp_dict = {'link': item.find('a')['href'], 'title': re.sub('\\s', '', item.find('a').get_text()),
-                     'rate': '评价人数不足' if len(rating_nums) == 0 else rating_nums[0].get_text(),
+                     'rate': '评价人数不足' if len(rating_nums) == 0 or rating_nums[0].get_text() == '' else
+                     rating_nums[0].get_text(),
                      'price': info_list[-1], 'pub_date': info_list[-2], 'press': info_list[-3],
                      'author': ' / '.join(info_list[:-3])}
         data_list.append(temp_dict)
     return data_list
 
 
+def save_as_json(page_data):
+    json_page_data = json.dumps(page_data, ensure_ascii=False)
+    f = open('page_data', 'w', encoding='utf8')
+    f.write(json_page_data)
+    f.close()
+
+
+def save_as_excel(page_data):
+    book = xlwt.Workbook(encoding='utf8')
+    sheet = book.add_sheet('心理学类信息', cell_overwrite_ok=True)
+    sheet.write(0, 0, '名称')
+    sheet.write(0, 1, '链接')
+    sheet.write(0, 2, '评分')
+    sheet.write(0, 3, '作者')
+    sheet.write(0, 4, '出版社')
+    sheet.write(0, 5, '出版日期')
+    sheet.write(0, 6, '价格')
+    for i in range(len(page_data)):
+        sheet.write(i + 1, 0, page_data[i]['title'])
+        sheet.write(i + 1, 1, page_data[i]['link'])
+        sheet.write(i + 1, 2, page_data[i]['rate'])
+        sheet.write(i + 1, 3, page_data[i]['author'])
+        sheet.write(i + 1, 4, page_data[i]['press'])
+        sheet.write(i + 1, 5, page_data[i]['pub_date'])
+        sheet.write(i + 1, 6, page_data[i]['price'])
+    book.save('心理学书籍信息.xls')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     start = time.time()
-    url = 'https://book.douban.com/tag/%E7%A4%BE%E4%BC%9A%E5%AD%A6?start=0&type=S'
+    url = 'https://book.douban.com/tag/%E5%BF%83%E7%90%86%E5%AD%A6?start=0&type=T'
     get_url_data(url)
     end = time.time()
     print("运行时间：" + str(end - start))
